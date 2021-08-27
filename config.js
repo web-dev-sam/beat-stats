6f7c5d9db3f5418a910661882cd68c9ccaa1c5ab
Samuel Braun <42311986+MindLaborDev@users.noreply.github.com>
2021-08-27 14:52:05 +0200

    as

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ document.getElementById('btn-scoresaber').addEventListener('click', evt => {
 		[$(`[name="scoresaber"]`).val()].join('|')
 	)
   
-  console.log("twitch.ext.configuration.broadcaster should exist:", twitch.ext.configuration);
+  console.log("twitch.ext.configuration.broadcaster should exist:", twitch.ext.configuration.broadcaster);
   document.getElementById('btn-scoresaber').style.display = 'none';
 })
 
@@ -21,5 +21,7 @@ hookOnGlobalConfigChanged(() => {
 	if (broadcasterConfig) {
     console.log("Data", twitch.ext.configuration.broadcaster);
 		let [id] = parseConfigStr(broadcasterConfig, [null])
-	}
+	} else {
+    console.log("TWITCH STUPID AND", $(`[name="scoresaber"]`).val());
+  }
 })
