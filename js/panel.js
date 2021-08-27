a3528538b8dbc3fbfa1cff4289d78f5d9ecc3a57
321rnr <jedisam321@gmail.com>
2021-08-27 16:56:48 +0100

    I did stuff :)

--- js/panel.js
+++ js/panel.js
@@ -12,8 +12,13 @@ hookOnGlobalConfigChanged(() => {
     // Get data and display everything
     getScoreSaberData(scoresaberId).then(playerData => {
       $("#name").text(playerData.playerName);
+      $("#country").text(playerData.country);
+      $("#flags").attr("src", "https://www.countryflags.io/" + playerData.country + "/shiny/16.png")
+      $("#profiePic").attr("src","https://new.scoresaber.com" + playerData.avatar);
       $("#rank").text(playerData.rank);
+      $("#countryRank").text(playerData.countryRank);
       $("#pp").text(playerData.pp);
+      $("#avgAcc").text( Math.round(playerData.avgAcc * 100) / 100);
     });
 
 	}
